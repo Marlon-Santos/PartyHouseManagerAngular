431b6e84224bb25460dedda0c0b1e9fa0c57f05f
Marlon Santos <marlonergondossantos@gmail.com>
2020-03-10 00:09:40 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TemplateModule } from './template/template.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { QueryService } from './template/service/query.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private service: QueryService) { }
+  ngOnInit(): void {
+    this.service.listApi("casas").subscribe(ev => {
+      console.log(ev);
+      this.api = ev;
+    });
+
+  }
   title = 'phm';
+  api;
 }
