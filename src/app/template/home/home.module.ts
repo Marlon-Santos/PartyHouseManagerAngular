c0f2fbff50be45a1de8f1481fbd4b8cebf445f89
marlon-santos <marlonergondossantos@gmail.com>
2020-03-11 15:53:27 -0300

    creating routes

--- src/app/template/home/home.module.ts
+++ src/app/template/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { SalesComponent } from "./sales/sales.component";
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SalesComponent, LoginComponent],
+  declarations: [HeaderComponent, SalesComponent, LoginComponent, HomeComponent],
   exports: [HeaderComponent, SalesComponent, LoginComponent],
   imports: [
     CommonModule
